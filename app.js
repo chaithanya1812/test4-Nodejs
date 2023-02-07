e16d76a506862b5572d1f316ed871869457f89c2
root <root@ip-172-31-34-31.ap-south-1.compute.internal>
2023-02-07 20:15:32 +0000

    second

---
+++ app.js
@@ -0,0 +1,36 @@
+//include http module 
+var http = require('http');
+
+http.createServer(function(req,res){
+	//store URL in variable q_string
+
+	var q_string = req.url;
+	switch(q_string) {
+		case '/':
+                        	res.writeHead(400, { 'Content-Type': 'text/plain' });
+                        	res.write("<h2>Welcome Chaithanya</h2>");
+                                res.write("<h2>/Node-JS Project</h2>");
+                        	res.end();
+                        	break;
+                	case '/about':
+                		res.writeHead(200, { 'Content-Type': 'text/plain' });
+                        	res.write('About Us');
+                        	res.write('\n\n');
+                        	res.write('Tecmint.com - Best Linux HowTos on the Web.');
+                        	res.write('\n');
+                        	res.end('Find out more: https://www.tecmint.com/who-we-are/');
+                        	break;
+                	case '/tecmint/authors':
+                        	res.writeHead(200, { 'Content-Type': 'text/plain' });
+                        	res.write('Tecmint Authors');
+                        	res.write('\n\n');
+                        	res.end('Find all our authors here: https://www.tecmint.com/who-we-are/');
+                        	break;
+                	default:
+                       		res.writeHead(404, { 'Content-Type': 'text/plain' });
+                       		res.end('Not Found');
+                        	break;
+	}
+}).listen(5555);
+console.log('Server started on localhost:5555; press Ctrl-C to terminate....');
+
